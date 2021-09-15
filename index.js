f57fa72c398c4d4a9e1dfeb1f9d3b4185bfb3092
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2021-09-15 11:31:28 -0400

    finished

--- index.js
+++ index.js
@@ -39,14 +39,29 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
 }
 
-
-
-
-
+// const mike = new Person('Mike', 28);
+// console.log(mike.toString());
+// mike.eat('wings')
+// console.log(mike.stomach);
+// mike.poop();
+// console.log(mike.stomach);
 
 
 /*
@@ -63,10 +78,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
 }
-
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
 
 /*
   TASK 3
@@ -75,18 +95,22 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ Person.call(this, name, age);
+ this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Global Object Binding. When no other rules apply this will return the window or global object
+  2. Implicit Binding. When a preceding dot calls a function the object before the dot is this.
+  3. Explicit Binding. Use .apply, .call, to explicitly call functions.
+  4. New Binding. When a constructor function is created this points to the new function.
 */
 
 
